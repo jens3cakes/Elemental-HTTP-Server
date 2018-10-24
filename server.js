2e9113432969ebcac9581d15697d1487a1d528b4
Jennifer Hikichi <jenniferhikichi@Jennifers-MBP.home>
2018-10-24 11:42:08 -1000

    GET is in a function

--- server.js
+++ server.js
@@ -4,56 +4,49 @@ const fs = require('fs');
 
 const PORT = process.env.PORT || 3000;
 
+function reqResp(uri, res) {
+  fs.readFile(`./public${uri}`, (err, data) => {
+    if (err) {
+      fs.readFile('./public/error.html', (err, data) => {
+        if (err) throw 'error';
+        res.setHeader('Content-Type', '[text/html, utf8]')
+        res.writeHead(404, 'ERROR')
+        res.write(data)
+        res.end();
+    }
+    )
+  }else {
+      res.setHeader('Content-Type', contType(uri))
+      res.writeHead('200', 'OK')
+      res.write(data)
+      res.end();
+    }
+  })
+}
+function contType(uri) {
+ const type = uri.split('.')
+return 'text/' + type[type.length-1]
+}
+
+
+
 const server = http.createServer((request, response) => {
   const uri = request.url;
   const reqMeth = request.method;
-console.log(uri)
-  function reqResp(uri, type) {
-    fs.readFile(`./public${uri}.html`, (err, data) => {
-      if (err) throw 'error';
-      response.setHeader('Content-Type', `text/${type}`)
-      response.writeHead('200', 'OK')
-      response.write(data)
-      response.end();
-    })
-  }
+  const res = response;
 
-  switch (uri) {
-    case '/hydrogen':
-    case '/hydrogen.html':
-      reqResp(uri,'html')
+  switch (reqMeth) {
+    case 'GET':
+      reqResp(uri, res);
       break;
-    case '/helium':
-    case '/helium.html':
-      reqResp(uri,'html')
-      break;
-    case '/':
-    case '/index.html':
-    case '/index':
-      reqResp(uri,'html')
-      break;
-    case '/css/styles.css':
-    reqResp(uri,'css')  
+    case 'POST':
     break;
     default:
-      fs.readFile('./public/error.html', (err, data) => {
-        if (err) throw 'error';
-        response.setHeader('Content-Type', '[text/html, utf8]')
-        response.write(404, 'ERROR')
-        response.write(data)
-        response.end();
-      })
       break;
   }
-  // const clientRequest = http.ClientRequest((request, response) => {
-  // response.setHeader(name, value),
-  // response.
-  // })
 })
 
 server.listen(PORT, () => {
   console.log(`Server is listening in on ${PORT}.`)
 });
-
-
 
