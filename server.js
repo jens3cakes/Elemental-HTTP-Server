863cfacb462418eb2ba7cec37224ec24b337018d
Jennifer Hikichi <jenniferhikichi@Jennifers-MBP.home>
2018-10-24 09:19:42 -1000

    working on the css uri response

--- server.js
+++ server.js
@@ -7,10 +7,11 @@ const PORT = process.env.PORT || 3000;
 const server = http.createServer((request, response) => {
   const uri = request.url;
   const reqMeth = request.method;
-  function reqHtmlResp(uri) {
+console.log(uri)
+  function reqResp(uri, type) {
     fs.readFile(`./public${uri}.html`, (err, data) => {
-      if (err) throw err;
-      response.setHeader('Content-Type', 'text/html')
+      if (err) throw 'error';
+      response.setHeader('Content-Type', `text/${type}`)
       response.writeHead('200', 'OK')
       response.write(data)
       response.end();
@@ -20,67 +21,23 @@ const server = http.createServer((request, response) => {
   switch (uri) {
     case '/hydrogen':
     case '/hydrogen.html':
-reqHtmlResp(uri)
+      reqResp(uri,'html')
       break;
-
-
-
-
-
-
-
-    //   if (err){console.log("error");
-    // }response.setHeader('Content-Type', `text/${contentType}`);
-    //   //response.setHeader('Content-Length', data.length);
-    //   response.writeHead(statusCode, statusStr)
-    //   response.write(data)
-    //   response.end();
-    // })
-    // };
-
-    // fs.readFile('public/hydrogen.html', (err, data) => {
-    //   if (err) throw 'error';
-    //   response.setHeader('Content-Type', '[text/html, utf8]')
-    //   response.writeHead(200, 'OK')
-    //   response.write(data)
-    //   response.end()
-    // })
-    // 
-    //reqResponse(`public${uri}`,'html',200,'OK')
     case '/helium':
     case '/helium.html':
-      fs.readFile('public/helium.html', (err, data) => {
-        if (err) throw 'error';
-        response.setHeader('Content-Type', '[text/html, utf8]')
-        response.writeHead(200, 'OK')
-        response.write(data)
-        response.end();
-      })
+      reqResp(uri,'html')
       break;
     case '/':
     case '/index.html':
     case '/index':
-      fs.readFile('public/index.html', (err, data) => {
-        if (err) throw 'error';
-        reqResponse(data)
-        response.setHeader('Content-Type', '[text/html, utf8]')
-        response.writeHead(200, 'OK')
-        response.write(data)
-        response.end();
-      })
+      reqResp(uri,'html')
       break;
     case '/css/styles.css':
-      fs.readFile('public/css/styles.css', (err, data) => {
-        if (err) throw 'error';
-        response.setHeader('Content-Type', '[text/css, utf8]')
-        response.writeHead(200, 'OK')
-        response.write(data)
-        response.end();
-      })
-      break;
+    reqResp(uri,'css')  
+    break;
     default:
-      fs.readFile('public/error.html', (err, data) => {
-        if (err) throw 'error'
+      fs.readFile('./public/error.html', (err, data) => {
+        if (err) throw 'error';
         response.setHeader('Content-Type', '[text/html, utf8]')
         response.write(404, 'ERROR')
         response.write(data)
@@ -92,19 +49,7 @@ reqHtmlResp(uri)
   // response.setHeader(name, value),
   // response.
   // })
-
-
-
-
 })
-
-
-
-
-
-
-
-
 
 server.listen(PORT, () => {
   console.log(`Server is listening in on ${PORT}.`)
